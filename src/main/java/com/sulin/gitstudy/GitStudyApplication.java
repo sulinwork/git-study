42972fc7c37bb234b143699ac0bfadf9074fb825
sulin <sulinwork@163.com>
2023-05-24 19:09:36 +0800

    test1

--- src/main/java/com/sulin/gitstudy/GitStudyApplication.java
+++ src/main/java/com/sulin/gitstudy/GitStudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudyApplication {
 
     public static void main(String[] args) {
+        //test1
         SpringApplication.run(GitStudyApplication.class, args);
     }
 
